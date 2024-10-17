34811860f61dea26f8d0d13552b8a3bd1f3908b7
Torn <21279212+TornOne@users.noreply.github.com>
2024-10-17 20:57:23 +0300

    Initial working version

---
+++ Config.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Config {
+	#region File options
+	public static FileInfo ffmpeg = new("ffmpeg.exe");
+	public static FileInfo[] inputFiles = [];
+	public static DirectoryInfo? outputDirectory;
+	public static bool createDirectoryIfNeeded = true;
+	public static string outputPrefix = "";
+	public static string outputSuffix = " out";
+	public static string outputExtension = ".webm";
+	public static bool overwrite = true;
+	#endregion
+
+	#region Video options
+	public static string videoEncoder = "libvpx-vp9";
+	#endregion
+
+	#region Audio options
+	public static string audioEncoder = "libopus";
+	#endregion
+
+	#region CPU options
+	public static ProcessPriorityClass priority = ProcessPriorityClass.BelowNormal;
+	public static nint cpuAffinity = 0;
+	#endregion
+
+	static readonly Dictionary<string, Action<string>> setters = new() {
+		#region File options
+		{ nameof(ffmpeg), path => {
+			ffmpeg = new(path);
+			if (!ffmpeg.Exists) {
+				throw new FileNotFoundException("ffmpeg not found", ffmpeg.FullName);
+			}
+		} },
+		{ nameof(inputFiles), paths => {
+			inputFiles = Array.ConvertAll(paths.Split('\n', StringSplitOptions.RemoveEmptyEntries), path => new FileInfo(path));
+			foreach (FileInfo input in inputFiles) {
+				if (!input.Exists) {
+					throw new FileNotFoundException("Input file not found", input.FullName);
+				}
+			}
+		} },
+		{ nameof(outputDirectory), path => outputDirectory = path == "" ? null : new(path)},
+		{ nameof(createDirectoryIfNeeded), value => createDirectoryIfNeeded = value == "true" },
+		{ nameof(outputPrefix), prefix => outputPrefix = prefix },
+		{ nameof(outputSuffix), suffix => outputSuffix = suffix },
+		{ nameof(outputExtension), extension => outputExtension = extension == "" ? "" : "." + extension },
+		{ nameof(overwrite), value => overwrite = value == "true" },
+		#endregion
+
+		#region Video options
+		{ nameof(videoEncoder), name => videoEncoder = name },
+		#endregion
+
+		#region Audio options
+		{ nameof(audioEncoder), name => audioEncoder = name },
+		#endregion
+
+		#region CPU options
+		{ nameof(priority), value => priority = Enum.Parse<ProcessPriorityClass>(value) },
+		{ nameof(cpuAffinity), value => cpuAffinity = value.StartsWith("0b") ? (nint)Convert.ToUInt64(value[2..], 2) : (1 << int.Parse(value)) - 1}
+		#endregion
+	};
+
+	static Config() {
+		Override(new FileInfo("defaults.cfg"));
+	}
+
+	public static void ReplaceInputFiles(string[] args) => setters[nameof(inputFiles)](string.Join('\n', args));
+
+	public static void Override(FileInfo file) {
+		if (!file.Exists) {
+			throw new FileNotFoundException("Configuration file not found", file.FullName);
+		}
+
+		int lineNo = 0;
+		foreach (string line in File.ReadLines(file.FullName)) {
+			lineNo++;
+
+			//Ignore empty lines and comments
+			if (line.Length == 0 || line[0] == '#') {
+				continue;
+			}
+
+			string[] parts = line.Split(['='], 2);
+			if (parts.Length < 2) {
+				throw CreateEx($"Expected to find a \"=\"");
+			}
+			string name = parts[0];
+			string value = parts[1].Replace("\\n", "\n"); //Only newlines can be escaped
+
+			if (!setters.TryGetValue(name, out Action<string>? Setter)) {
+				throw CreateEx($"Invalid setting name: \"{name}\"");
+			}
+
+			try {
+				Setter(value);
+			} catch (Exception ex) {
+				throw CreateEx($"Failed to validate value for setting \"{name}\"", ex);
+			}
+		}
+
+		Exception CreateEx(string msg, Exception? ex = null) => new($"Error in {file.Name} line {lineNo}:\n{msg}", ex);
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+static class Program {
+	static void Main(string[] args) {
+		Environment.CurrentDirectory = Path.GetDirectoryName(Environment.ProcessPath)!;
+
+		//If the first argument is a config file, use it as an override and remove it from the arguments
+		if (args.Length > 0) {
+			FileInfo file = new(args[0]);
+			if (file.Exists && file.Extension == ".cfg") {
+				Config.Override(file);
+				args = args[1..];
+			}
+		}
+
+		//The remaining argments are expected to be input media files
+		if (args.Length > 0) {
+			Config.ReplaceInputFiles(args);
+		}
+
+		foreach (FileInfo input in Config.inputFiles) {
+			//Global options
+			List<string> arguments = [ "-hide_banner" ];
+			if (Config.overwrite) {
+				arguments.Add("-y");
+			}
+
+			//Input options
+
+			//Input file
+			arguments.Add("-i");
+			arguments.Add(input.FullName);
+
+			//Output options
+			if (Config.videoEncoder == "") {
+				arguments.Add("-vn");
+			} else {
+				arguments.Add("-c:v");
+				arguments.Add(Config.videoEncoder);
+			}
+
+			if (Config.audioEncoder == "") {
+				arguments.Add("-an");
+			} else {
+				arguments.Add("-c:a");
+				arguments.Add(Config.audioEncoder);
+			}
+
+			//Output file
+			if (Config.outputDirectory is not null) {
+				if (Config.createDirectoryIfNeeded) {
+					Config.outputDirectory.Create();
+				} else if (!Config.outputDirectory.Exists) {
+					throw new Exception("Output directory does not exist");
+				}
+			}
+			FileInfo output = new($"{(Config.outputDirectory ?? input.Directory!).FullName}/{Config.outputPrefix}{Path.GetFileNameWithoutExtension(input.Name)}{Config.outputSuffix}{Config.outputExtension}");
+			if (Array.Exists(Config.inputFiles, input => input.FullName == output.FullName)) {
+				throw new Exception("Output would overwrite an input file");
+			}
+			arguments.Add(output.FullName);
+
+			Console.WriteLine(Config.ffmpeg.FullName + ' ' + string.Join(' ', arguments));
+			ProcessStartInfo startInfo = new(Config.ffmpeg.FullName, arguments);
+			//Console.WriteLine(Config.ffmpeg.FullName + ' ' + startInfo.Arguments);
+			//continue;
+			Process ffmpegProcess = Process.Start(startInfo)!;
+			ffmpegProcess.PriorityClass = Config.priority;
+			if (Config.cpuAffinity != 0 && (OperatingSystem.IsWindows() || OperatingSystem.IsLinux())) {
+				ffmpegProcess.ProcessorAffinity = Config.cpuAffinity;
+			}
+			//TODO: Track the conversion process and update the output (including the ETA)
+			ffmpegProcess.WaitForExit();
+		}
+	}
+}
